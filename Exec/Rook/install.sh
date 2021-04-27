c4500eb282bcd4db539f4d3428aa2018258b0316
mbizhani <mbizhani@gmail.com>
2021-04-27 11:36:16 +0430

    CephFS - both replication 1 & 3

--- Exec/Rook/install.sh
+++ Exec/Rook/install.sh
@@ -14,15 +14,20 @@ kubectl create namespace rook-ceph
 helm install rook-ceph Helm/rook-ceph/ --namespace rook-ceph -f Helm/values.yml
 waitForPod "app=rook-ceph-operator"
 
-kubectl apply -f Cluster/cluster-dev.yml
+ENV=${1:-Dev}
+echo "ENV = ${ENV}"
+
+kubectl apply -f Cluster/${ENV}/cluster.yml
 waitForPod "app=rook-ceph-mgr"
 
+kubectl apply -f Cluster/${ENV}/fs.yml
+if [ "${ENV}"=="Prd" ]; then
+  kubectl apply -f Cluster/Prd/fs-r1-storageclass.yml -f Cluster/Prd/fs-r3-storageclass.yml
+else
+  kubectl apply -f Cluster/Dev/fs-storageclass.yml
+fi
+
 kubectl apply -f Cluster/dashboard-ingress.yml
-
-kubectl apply -f Cluster/fs-dev.yml
-
-kubectl apply -f Cluster/fs-storageclass.yml
-
 kubectl apply -f Cluster/toolbox.yml
 waitForPod "app=rook-ceph-tools"
 kubectl -n rook-ceph exec -it $(kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}') -- bash
